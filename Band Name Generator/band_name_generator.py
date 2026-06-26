ca378e3b4879a6e4f58637fdf25e12c60d4500a3
Gokula sankara pandi <madgokul85@gmail.com>
2026-06-26 21:22:21 +0530

    Update band_name_generator.py

--- Band Name Generator/band_name_generator.py
+++ Band Name Generator/band_name_generator.py
@@ -1,1 +1,15 @@
+def generate_band_name():
+    print("Welcome to the Band Name Generator")
 
+    city_name = input("What's your city you grew up in! ").strip()
+    pet_name = input("What's your pet name? ").strip()
+
+    band_name = f"{city_name.title()} {pet_name.title()}"
+
+    print(f"Your band name is {band_name}")
+
+    if not city_name or not pet_name:
+        print("Please enter your valid city and pet name!")
+        return
+
+generate_band_name()
